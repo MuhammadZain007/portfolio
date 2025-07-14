2c660d1c235c505f0afe4b57db20cb821e7cc476
MUHAMMAD ZAIN ALI HAIDER <zainawanawan61@gmail.com>
2025-07-14 22:31:00 +0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,5 @@
+// Simple JS form handler (no backend)
+document.getElementById("contactForm").addEventListener("submit", function (e) {
+  e.preventDefault();
+  alert("Message sent! (Functionality is demo only)");
+});
